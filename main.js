d42d175e156c26e727b1f31d92e77a5001444ce5
omarisayed <osayedunoff@gmail.com>
2023-03-15 12:07:18 +0300

    Added the functionality of CLEAR and BACKSPACE buttons.

--- main.js
+++ main.js
@@ -1,13 +1,30 @@
-let display = document.querySelector('.display-div')
+let screen = document.querySelector('.screen-div')
 const acButton = document.querySelector('.btn-ac')
+const backspaceButton = document.querySelector('.btn-del')
 const equalButton = document.querySelector('.btn-equal')
 const buttons = Array.from(document.querySelectorAll('.btn'))
 
 buttons.forEach(function(button){
-    button.addEventListener('click',function(e){
-        let num = e.target.dataset.val
-        display.textContent += num
+    button.addEventListener('click',function(e){ 
+        let value = e.target.dataset.val // check which button has been clicked on then display its value on screen
+        screen.textContent += value
     })
+})
+
+acButton.addEventListener('click', function(e){
+    screen.textContent = screen.textContent = ''
+})
+
+backspaceButton.addEventListener('click', function(e){
+    screen.textContent = screen.textContent.slice(0,-1);
+})
+
+equalButton.addEventListener('click', function(e){
+    if (screen.textContent === ''){  // if the screen if empty and you press = sign dont display anything.
+        screen.textContent = ''
+    }else{
+       
+    }
 })
 
 
