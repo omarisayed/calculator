d3d49c7db9858591feb6eac48f118ce9d6d9c72a
omarisayed <osayedunoff@gmail.com>
2023-03-16 16:30:13 +0300

    Added functionalities to buttons AC and =

--- main.js
+++ main.js
@@ -7,16 +7,55 @@ let result = "";
 function handleClick(button){
 
     if (button >= '0' && button <= '9'){ // If the button is numerical, and operator is an empty value, keep concatenating:
-        if (operator = ""){
+        if (operator === ""){
             num1 += button   // the numerical string values will concatenate, so we can have double/triple digits.
         }else {
             num2 += button  //else if operator is not empty, we move to num2(operand 2)
         }
-
-    }else if (button === '+' || button === '-' || button === 'x' || button === '÷'){ // if the button is an operator, assign the clicked operator button to the operator variable:
-        operator = button
-    }
     
-    screen.textContent = num1 + operator + num2
+    }else if (button === '.' && !((operator === "" && num1.includes(".")) || (operator !== "" && num2.includes(".")))){
+        // Add decimal point only if the number being entered doesnt already have one
+        if (operator === ""){
+            num1 += button;
+        }else{
+            num2 += button;
+        }
+    }// if the button is an operator, assign the clicked operator button to the operator variable:
+    else if (button === '+' || button === '-' || button === 'x' || button === '÷'){ 
+        operator = button
+
+    } // if button clicked is = :
+    else if (button === "="){  
+        if (operator === "+"){
+            result = parseFloat(num1) + parseFloat(num2)
+        }else if (operator === "-"){
+            result = parseFloat(num1) - parseFloat(num2)
+        }else if (operator === "x"){
+            result = parseFloat(num1) * parseFloat(num2)
+        }else if (operator === "÷"){
+            result = parseFloat(num1) / parseFloat(num2)
+        }
+
+        if (Math.round(result) === result){
+            result = result.toFixed(0); //display as integer
+        }else{
+            result = result.toFixed(2); // display with two decimal places
+        }
+        // Reset variables for the next calculation
+        num1 = result.toString()
+        num2 = "";
+        operator = "";
+        result = ""; 
+
+    } // If the button clicked is AC:
+    else if (button === 'AC'){
+        num1 = "";
+        num2 = "";
+        operator = "";
+        result = "";
+    }
+
+    screen.textContent = num1 + operator + num2 + result.toString();
+    
 }
 
